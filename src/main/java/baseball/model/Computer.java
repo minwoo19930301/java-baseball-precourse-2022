9efa3887cff18b4ef264bbc25feff5739d95f62c
김민우 <rlaalsdn4564@hanmail.net>
2022-10-03 04:54:42 +0900

    fix : need to use pickNumberInRange to pass test

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -2,7 +2,8 @@ package baseball.model;
 import static baseball.controller.BaseballGuessingGame.COUNT_OF_BALLS;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author: Minwoo Kim
@@ -14,14 +15,22 @@ public class Computer extends Player {
         initBalls();
     }
 
-    @Override
-    public void initBalls() {
-        List<Integer> randInts = Randoms.pickUniqueNumbersInRange(0, 9, COUNT_OF_BALLS);
+    private int generateUniqueDigitedNumber(){
+        Set<Integer> unique = new HashSet<>();
+        while(unique.size()<COUNT_OF_BALLS){
+            unique.add(Randoms.pickNumberInRange(0,9));
+        }
         StringBuilder temp = new StringBuilder();
-        for(Integer integer : randInts){
+        for(Integer integer : unique){
             temp.append(integer.toString());
         }
-        this.getBalls().setBallNums(Integer.parseInt(temp.toString()));
+        return Integer.parseInt(temp.toString());
+    }
+
+    @Override
+    public void initBalls() {
+        int uniqueDigitNum = generateUniqueDigitedNumber();
+        this.getBalls().setBallNums(uniqueDigitNum);
     }
 
 }
