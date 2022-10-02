873d06a922e81dc6a2e552152adfcfaead239492
김민우 <rlaalsdn4564@hanmail.net>
2022-10-03 01:12:44 +0900

    feat : create launcher

---
+++ src/main/java/baseball/controller/BaseballGuessingGame.java
@@ -0,0 +1,19 @@
+package baseball.controller;
+
+/**
+ * @author: Minwoo Kim
+ * @date 2022/10/02
+ */
+
+public class BaseballGuessingGame implements BaseballGame {
+
+
+    public BaseballGuessingGame() {
+    }
+
+
+    @Override
+    public void start() {
+
+    }
+}
