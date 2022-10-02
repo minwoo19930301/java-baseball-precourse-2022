2245bcfc4f02cca42b807759b3dcc62d1996d092
김민우 <rlaalsdn4564@hanmail.net>
2022-10-03 02:45:30 +0900

    feat : init user with exceptions

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,9 +1,30 @@
 package baseball.model;
 
+import static baseball.controller.BaseballGuessingGame.COUNT_OF_BALLS;
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * @author: Minwoo Kim
  * @date 2022/10/03
  */
-public class User extends Player{
+public class User extends Player {
+
+    private void validateInput(String input) {
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    //a constructor like method intended to call multiple times
+    @Override
+    public void initBalls() {
+        String input = Console.readLine();
+        validateInput(input);
+        this.getBalls().setBallNums(Integer.parseInt(input));
+    }
+
 
 }
