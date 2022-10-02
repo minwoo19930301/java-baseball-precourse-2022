f3a04a6552bd59f06cdb80bfdee1d5b555cecd1b
김민우 <rlaalsdn4564@hanmail.net>
2022-10-03 04:44:58 +0900

    feat : complete game with restart logic

--- src/main/java/baseball/controller/BaseballGuessingGame.java
+++ src/main/java/baseball/controller/BaseballGuessingGame.java
@@ -5,6 +5,8 @@ import baseball.model.BallsMatch;
 import baseball.model.Computer;
 import baseball.model.Player;
 import baseball.model.User;
+import baseball.view.Output;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * @author: Minwoo Kim
@@ -16,10 +18,21 @@ public class BaseballGuessingGame implements BaseballGame {
     public static final String GAME_LANGUAGE = "KOR";
     public static final int COUNT_OF_BALLS = 3;
     public static final int OBJECTIVE_MATCH = 3;
+    public static final String RESTART_YES = "1";
+    public static final String RESTART_NO = "2";
 
     @Override
     public void start() {
-        restartableGame();
+        String input;
+        do{
+            restartableGame();
+            System.out.println(Output.RESTART_INQUIRY.getOutput());
+            input = Console.readLine();
+            if(!(input.equals(RESTART_YES) || input.equals(RESTART_NO))){
+                throw new IllegalArgumentException();
+            }
+        }while(input.equals(RESTART_YES));
+
     }
 
     public void restartableGame(){
