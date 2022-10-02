63a83618e33af610f4da3112a7e34ff8ecc6b8fd
김민우 <rlaalsdn4564@hanmail.net>
2022-10-02 21:45:07 +0900

    chore : init architecture with launcher

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,12 @@
+package baseball;
+
+/**
+ * @author: Minwoo Kim
+ * @date 2022/10/02
+ */
+
+//made for reusing project for other baseball games
+public interface BaseballGame {
+
+    void start();
+}
---
+++ src/main/java/baseball/BaseballGuessingGame.java
@@ -0,0 +1,17 @@
+package baseball;
+
+
+/**
+ * @author: Minwoo Kim
+ * @date 2022/10/02
+ */
+public class BaseballGuessingGame implements BaseballGame {
+
+    public BaseballGuessingGame() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
