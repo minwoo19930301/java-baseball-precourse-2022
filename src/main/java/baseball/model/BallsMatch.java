dbec98939051dd7adc956c6a5dc018c3e6e7ac72
김민우 <rlaalsdn4564@hanmail.net>
2022-10-03 04:18:05 +0900

    feat : calculate ball, strikes

--- src/main/java/baseball/model/BallsMatch.java
+++ src/main/java/baseball/model/BallsMatch.java
@@ -1,5 +1,10 @@
 package baseball.model;
 
+import static baseball.controller.BaseballGuessingGame.COUNT_OF_BALLS;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author: Minwoo Kim
  * @date 2022/10/03
@@ -9,16 +14,33 @@ public class BallsMatch {
     private int strike_count;
     private int ball_count;
 
-    private void count_strikes(Balls balls1, Balls balls2){
-
+    private void count_strikes(int[] ballNums1, int[] ballsNums2) {
+        for (int i = 0; i < COUNT_OF_BALLS; i++) {
+            if (ballNums1[i] == ballsNums2[i]) {
+                strike_count++;
+            }
+        }
     }
 
-    private void count_balls(Balls balls1, Balls balls2){
-
+    private void count_balls(int[] riddlerBallNums, int[] guesserBallsNums) {
+        Set<Integer> set = new HashSet();
+        for (int i = 0; i < COUNT_OF_BALLS; i++) {
+            set.add(riddlerBallNums[i]);
+        }
+        for (int j = 0; j < COUNT_OF_BALLS; j++) {
+            if (set.contains(guesserBallsNums[j])) {
+                ball_count++;
+            }
+        }
+        if (ball_count > 0) {
+            ball_count -= strike_count;
+        }
     }
 
-    public void matchBalls(){
-
-
+    public void matchAllBalls(Player riddler, Player guesser) {
+        int[] riddlerBallNums = riddler.getBalls().getBallNums();
+        int[] guesserBallNums = guesser.getBalls().getBallNums();
+        count_strikes(riddlerBallNums, guesserBallNums);
+        count_balls(riddlerBallNums, guesserBallNums);
     }
 }
